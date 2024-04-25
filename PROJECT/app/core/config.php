9009728b627645a5415f83660973421e867c2cb6
RaisanParaiso <raisanparaiso00@gmail.com>
2024-04-25 21:22:33 +0800

    update

---
+++ PROJECT/app/core/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('ROOT', 'http://localhost/bsis3f_weba_24/mvc-test/public');
+define('PATH', 'C:/xampp/htdocs/BSIS3F_WEBA_24/mvc-test/app/views/');
+
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'mvc_db');
+define('DB_USER', 'root');
+define('DB_PASS', '');
